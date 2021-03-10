579e2afc1dd4d6950a0d61ed8f1996b55cfcf457
Jude Taylor <me@jude.xyz>
2021-03-10 11:30:55 -0800

    skip cc step for wasm32 arch

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 extern crate cc;
 
 fn main() {
-    if !cfg!(feature = "no_cc") {
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").expect("no TARGET_ARCH set");
+
+    if cfg!(not(feature = "no_cc")) && target_arch != "wasm32" {
         cc::Build::new()
             .file("src/hide.c")
             .compile("clear_on_drop");
